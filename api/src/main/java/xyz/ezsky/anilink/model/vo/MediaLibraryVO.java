f5768bf4f78a2ecba5affd066d7c8f8f312ae849
ezsky111 <ezsky111@gmail.com>
2025-11-10 07:20:07 +0000

    【修改】(媒体库): 更新MediaLibraryVO和MediaLibraryService类，添加媒体库状态字段及相关方法，增强功能和文档说明

--- api/src/main/java/xyz/ezsky/anilink/model/vo/MediaLibraryVO.java
+++ api/src/main/java/xyz/ezsky/anilink/model/vo/MediaLibraryVO.java
@@ -2,6 +2,7 @@ package xyz.ezsky.anilink.model.vo;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+import xyz.ezsky.anilink.model.entity.MediaLibrary;
 
 @Data
 @Schema(description = "媒体库视图对象，用于展示媒体库信息")
@@ -12,4 +13,6 @@ public class MediaLibraryVO {
     private String name;
     @Schema(description = "媒体库的物理路径", example = "/data/anime")
     private String path;
+    @Schema(description = "媒体库当前状态", example = "OK")
+    private MediaLibrary.Status status;
 }
